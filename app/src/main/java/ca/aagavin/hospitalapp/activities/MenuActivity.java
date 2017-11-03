3cf1b761e3a07a93e88958aca5ecf2caf0b6ade5
aagavin <aagavin@gmail.com>
2017-11-03 17:41:39 -0400

    added options menu activity

---
+++ app/src/main/java/ca/aagavin/hospitalapp/activities/MenuActivity.java
@@ -0,0 +1,37 @@
+package ca.aagavin.hospitalapp.activities;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import ca.aagavin.hospitalapp.R;
+
+public class MenuActivity extends AppCompatActivity implements AdapterView.OnItemClickListener{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        ((ListView) findViewById(R.id.options)).setOnItemClickListener(this);
+    }
+
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long index) {
+
+        switch ((int) index){
+            case 0:
+                break;
+            case 1:
+                startActivity(new Intent(this, TestDataActivity.class));
+                break;
+            default:
+                break;
+        }
+    }
+}
