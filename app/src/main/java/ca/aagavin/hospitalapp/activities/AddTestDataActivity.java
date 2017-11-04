8b66c9f736d8c327c7d3efb77b630dc4eb5ddab4
aagavin <aagavin@gmail.com>
2017-11-03 22:36:24 -0400

    layout

--- app/src/main/java/ca/aagavin/hospitalapp/activities/AddTestDataActivity.java
+++ app/src/main/java/ca/aagavin/hospitalapp/activities/AddTestDataActivity.java
@@ -2,6 +2,8 @@ package ca.aagavin.hospitalapp.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
 import ca.aagavin.hospitalapp.R;
 
@@ -11,5 +13,9 @@ public class AddTestDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_data);
+    }
+
+    public void saveTestBtn(View view) {
+        Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
 }
