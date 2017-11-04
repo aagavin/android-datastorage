f21cd42cc271de6b285b5a452e2a8e17d73e0eef
aagavin <aagavin@gmail.com>
2017-11-03 20:59:50 -0400

    renamed file

--- app/src/main/java/ca/aagavin/hospitalapp/activities/AddTestDataActivity.java
+++ app/src/main/java/ca/aagavin/hospitalapp/activities/AddTestDataActivity.java
@@ -5,7 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 
 import ca.aagavin.hospitalapp.R;
 
-public class TestDataActivity extends AppCompatActivity {
+public class AddTestDataActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
