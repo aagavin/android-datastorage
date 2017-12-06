734d132df89cac837b9a179316b7d93ec76d3ba6
aagavin <aagavin@gmail.com>
2017-12-06 18:21:20 -0500

    more code

--- app/src/main/java/ca/aagavin/hospitalapp/activities/EditPatientActivity.java
+++ app/src/main/java/ca/aagavin/hospitalapp/activities/EditPatientActivity.java
@@ -2,7 +2,10 @@ package ca.aagavin.hospitalapp.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
@@ -12,9 +15,11 @@ import ca.aagavin.hospitalapp.DAO.CommonDAO;
 import ca.aagavin.hospitalapp.R;
 import ca.aagavin.hospitalapp.beans.Patient;
 
-public class EditPatientActivity extends AppCompatActivity {
+public class EditPatientActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private CommonDAO _dao;
+    private List<Patient> _allPatients;
+    private int _selectedIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,11 @@ public class EditPatientActivity extends AppCompatActivity {
 
         this._dao = new CommonDAO(this);
 
-        List<Patient> a =  this._dao.getAllPatients();
+        this._allPatients =  this._dao.getAllPatients();
         List<String> dropDown = new ArrayList<>();
 
 
-        for (Patient p :a) {
+        for (Patient p : this._allPatients) {
             System.out.println(p.getFirstname() +" " + p.getLastname());
             dropDown.add(p.getFirstname() +" " + p.getLastname());
         }
@@ -38,8 +43,7 @@ public class EditPatientActivity extends AppCompatActivity {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         spinner.setAdapter(adapter);
-
-
+        spinner.setOnItemSelectedListener(this);
 
     }
 
@@ -50,4 +54,19 @@ public class EditPatientActivity extends AppCompatActivity {
 
         this._dao.close();
     }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        this._selectedIndex = i;
+
+        Patient selectedPatient = this._allPatients.get(i);
+
+        ((EditText) findViewById(R.id.editTextfirstname)).setText(selectedPatient.getFirstname());
+        ((EditText) findViewById(R.id.editTextlastname)).setText(selectedPatient.getFirstname());
+        ((EditText) findViewById(R.id.editTextdepartment)).setText(selectedPatient.getDepartment());
+        ((EditText) findViewById(R.id.editTextroom)).setText(selectedPatient.getRoom());
+
+    }
+
+    @Override public void onNothingSelected(AdapterView<?> adapterView) {}
 }
